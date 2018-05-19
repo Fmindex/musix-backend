4f8502609b64a3fbaa79c2e02d1a759998daae17
Ekkalak Leelasornchai <ekkalakfm@gmail.com>
2018-05-20 04:02:33 +0700

    EFEW

--- app.py
+++ app.py
@@ -1,16 +1,47 @@
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
+import os
+from flask import Flask, request, redirect, url_for, Response
+from werkzeug.utils import secure_filename
+from flask_restful import Resource, Api
+import json
 
+
+UPLOAD_FOLDER = 'upload/'
+ALLOWED_EXTENSIONS = set(['wav'])
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 api = Api(app)
-parser = reqparse.RequestParser()
-parser.add_argument('bofy')
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 class HelloWorld(Resource):
+	def get(self):
+		response = Response(
+		    response=json.dumps(dict(err='no file')), 
+		    status=400, mimetype='application/json')
+		return response
+
 	def post(self):
-		args = parser.parse_args()
-		print(args)
-		return "POST", 200
+		print(request.form)
+		if 'file' not in request.files:
+		    print('No file part')
+		    return redirect(request.url)
+		file = request.files['file']
+		# if user does not select file, browser also
+		# submit a empty part without filename
+		if file.filename == '':
+		    print('No selected file')
+		    return redirect(request.url)
+		if file and allowed_file(file.filename):
+		    filename = secure_filename(file.filename)
+		    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		    return redirect(url_for('uploaded_file',
+		                            filename=filename))
+		response = Response(
+		    response=json.dumps(dict(kuy='nivit')), 
+		    status=400, mimetype='application/json')
+		return response
+
 
 api.add_resource(HelloWorld, '/')
 
